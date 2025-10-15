d59ad26a99298424178ce68df2b3a6d7a9698d05
SHRAY SALVI <80808215+shraysalvi@users.noreply.github.com>
2025-10-15 16:52:46 +0530

    Delete alembic directory
    Delete api directory
    
    Delete core directory
    
    Delete docs directory
    
    Delete requirements directory
    
    Delete services directory
    
    Delete README.md
    
    Delete .gitignore
    
    Delete alembic.ini
    
    Delete main.py

--- core/connections/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
-from core.config import settings
-
-engine=create_engine(settings.DATABASE_URL,future=True,echo=False)
-
-Base=declarative_base()
